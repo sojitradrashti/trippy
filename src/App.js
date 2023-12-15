89b76c0eafd3f9ddc9c548c17c721b3e5782c7d7
sojitradrashti <sojitradrashti5@gmail.com>
2023-12-15 09:03:09 -0800

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Navbar from "./component/Navbar";
+import { Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div class="App">
+      <Navbar />
+    </div>
+  );
+}
